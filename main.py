3214a748900bc208428e47dfdc497f9f9207881c
iceeburr <iceeburr@gmail.com>
2023-06-23 18:26:09 +0300

    Main push to the repo.

---
+++ main.py
@@ -0,0 +1,73 @@
+import rblxopencloud
+import os
+from pathlib import Path
+
+creator = rblxopencloud.Group(
+    GROUP_ID, api_key="API_KEY" # Change this to your API key. Don't forget to change the Group ID as well. If you use your own account change .Group to .User and Group ID would be your User ID.
+)
+
+content = "local FrameIDs = {\n"
+assetIDsArray = []
+
+def uploadAsset(currentFile):
+    with open(currentFile, "rb") as file:
+        asset = creator.upload_asset(
+            file, rblxopencloud.AssetType.Decal, Path(currentFile).stem, "Decal Description" # Make sure your file names don't contain anything that can get moderated. Use "frame00001" or something similar.
+        )
+        if isinstance(asset, rblxopencloud.Asset):
+            print(asset.id, Path(currentFile).stem)
+            assetIDsArray.append(asset.id)
+        else:
+            while True:
+                status = asset.fetch_status()
+                if status:
+                    print(status.id, Path(currentFile).stem)
+                    assetIDsArray.append(status.id)
+                    break
+
+def mainFunction():
+    fileDirectory = './files' # If your directory is named differently please edit this.
+    for fileName in os.scandir(fileDirectory):
+        if fileName.is_file():
+            try:
+                uploadAsset(fileName.path)
+            except Exception as e:
+                for i in range(len(assetIDsArray)):
+                    content += "   " + str(assetIDsArray[i]) + ",\n"
+                content += "}"
+                with open('./output.txt', 'w') as file:
+                    file.write(content)
+                    file.close()
+                print(e)
+                exit("Something went wrong. Your image might have been moderated. All images have been saved to output.txt. Please read the error. If your content was moderated then you will have to check that out yourself.")
+
+    if len(assetIDsArray) == 0:
+        exit("No files have been uploaded. Are you sure you have any files in the folder?")
+    for i in range(len(assetIDsArray)):
+        content += "   " + str(assetIDsArray[i]) + ",\n"
+    content += "}"
+
+    with open('./output.txt', 'w') as file:
+        file.write(content)
+        file.close()
+    print("The code ran successfully, all image ID's should be in the file output.txt. Good luck!")
+
+if os.path.exists('./output.txt'):
+  print("You currently have a output.txt file. If you continue it will be deleted completely and you won't be able to recover it's contents.")
+  inputOutput = input("Do you want to continue? [Y/N] ").lower()
+  if inputOutput == "y":
+      os.remove('./output.txt')
+      mainFunction()
+  elif inputOutput == "n":
+      exit()
+  else:
+      exit('That is not a valid answer! Please pick between "Y" (Yes) and "N" (No).')
+else:
+  print("No output.txt was detected. A new file will be created in the home directory of the python file.")
+  inputOutput = input("Do you want to continue? [Y/N] ").lower()
+  if inputOutput == "y":
+      mainFunction()
+  elif inputOutput == "n":
+      exit()
+  else:
+      exit('That is not a valid answer! Please pick between "Y" (Yes) and "N" (No).')
